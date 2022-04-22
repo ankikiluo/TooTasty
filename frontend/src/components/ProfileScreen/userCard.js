2f90ee2f1486f8fad54c5f8013a565fe89b796be
ankikiluo <luo.an@northeastern.edu>
2022-04-22 09:47:18 -0400

    remove following and followed

--- frontend/src/components/ProfileScreen/userCard.js
+++ frontend/src/components/ProfileScreen/userCard.js
@@ -6,15 +6,17 @@ const UserCard = () => {
             <div className="user-card user-card-flex">
                 <div className="m-5">
                     <img src="./img/top-liked-1.jpg" alt=""/>
-                    <h3 className="user-card-name text-white">Alice <button type="button" className="btn btn-primary">Follow</button></h3>
+                    <h3 className="user-card-name text-white">Alice
+                        {/*<button type="button" className="btn btn-primary">Follow</button>*/}
+                    </h3>
                     <p className="text-white">I love eatting!</p>
 
-                    <ul className="list-group mb-4">
-                        <li className="list-group-item"><Link to="#">Follower</Link></li>
-                        <li className="list-group-item"><Link to="#">Following</Link></li>
+                    {/*<ul className="list-group mb-4">*/}
+                        {/*<li className="list-group-item"><Link to="#">Follower</Link></li>*/}
+                        {/*<li className="list-group-item"><Link to="#">Following</Link></li>*/}
                         {/*<li className="list-group-item"><Link to="#">Reviews</Link></li>*/}
                         {/*<li className="list-group-item"><Link to="#">Favourite restaurants</Link></li>*/}
-                    </ul>
+                    {/*</ul>*/}
                     <button type="reset" className="btn btn-outline-white">Edit Profile</button>
                 </div>
             </div>
